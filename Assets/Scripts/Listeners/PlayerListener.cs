0377472be9bdc0886349e521eab234cde7dca050
Federick <federickg23@gmail.com>
2019-03-04 15:13:06 -0500

    Edited the code. Now, both players are able to input key presses at the same time.

--- Assets/Scripts/Listeners/PlayerListener.cs
+++ Assets/Scripts/Listeners/PlayerListener.cs
@@ -8,19 +8,36 @@ public class PlayerListener : KeyListener {
 	//called by GameLoop
 	//RegistersCurrentKeyHits
 
+    private bool isKeyEnabled = false;
+
     override public string GetInput(){
         //string temp = lastPressed;
         //lastPressed = "";
         //return temp;
         string str = "";
-        foreach(KeyCode kcode in Enum.GetValues(typeof(KeyCode))) {
-            if(Input.GetKeyDown(kcode))
-                str += kcode;
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+
+            if (Input.GetKeyDown(kcode))
+            {
+                if ( Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.Comma) || Input.GetKeyDown(KeyCode.O) || Input.GetKeyDown(KeyCode.M) || Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.N) || Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.K)){
+                     isKeyEnabled = false;
+                }
+                else{
+                    isKeyEnabled = true;
+                }
+
+                if (isKeyEnabled)
+                {
+                    str += kcode;
+                }
+            }
         }
         if (!isEnabled) return "";
         return str;
     }
 
-    public void Update() {
+    public void Update()
+    {
     }
 }
--- Assets/Scripts/Listeners/PlayerListener2.cs
+++ Assets/Scripts/Listeners/PlayerListener2.cs
@@ -4,22 +4,35 @@ using System;
 public class PlayerListener2 : KeyListener
 {
 
+
     //called by GameLoop
     //RegistersCurrentKeyHits
     readonly string[] forbiddenKeys = { "Q", "W", "E", "A", "S", "D", "Z", "X", "C" };
+
+    private bool isKeyEnabled = false;
+
     override public string GetInput()
     {
         //string temp = lastPressed;
         //lastPressed = "";
         //return temp;
         string str = "";
+
+
+
         foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
         {
 
             if (Input.GetKeyDown(kcode))
             {
-                int pos = Array.IndexOf(forbiddenKeys, Input.inputString);
-                if (pos == -1)
+                if ( Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.C)){
+                     isKeyEnabled = false;
+                }
+                else{
+                    isKeyEnabled = true;
+                }
+
+                if (isKeyEnabled)
                 {
                     str += kcode;
                 }
